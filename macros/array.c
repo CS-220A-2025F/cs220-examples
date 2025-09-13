f2c8630e00f507718eb767d6374a3040f61bfd74
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-09-12 20:23:30 -0400

    update

--- macros/array.c
+++ macros/array.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 10
-#define PROD(m,n) m*n
+#define SIZE 20
+#define PROD(m,n) ((m)*(n))
+#define POW(n) (1UL<<(n))
 
 int main(){
   int ary[SIZE];
@@ -11,6 +12,7 @@ int main(){
   }
   for (int i = 0; i < SIZE; i++){
     // alter the array here
+    ary[i] = POW(ary[i]);
     printf("ary[%d] = %d\n", i, ary[i]);
   }
   return 0;
