4c5a450589f6cda9c437aa6c8cd505ee6c6aa453
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-09-17 11:24:33 -0400

    added example

---
+++ xor_swap/swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *num1, int *num2){
+  //TODO: swap *num1 and *num2 without using a temp variable 
+}
+
+int main(){
+  int a = 100;
+  int b = 200;
+  printf("a = %d, b = %d\n", a, b);
+  swap(&a, &b);
+  printf("a = %d, b = %d\n", a, b);
+}
