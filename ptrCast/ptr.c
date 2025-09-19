5f90b5181a955186c4726a83adacf0da5dc84397
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-09-19 13:49:43 -0400

    update

--- ptrCast/ptr.c
+++ ptrCast/ptr.c
@@ -9,16 +9,16 @@ int main(){
   int *ptr_i = ptr_v;
   long *ptr_l = (long *) ptr_i;
 
-  // TODO: set the values to which ptr_i and
+  // set the values to which ptr_i and
   // ptr_i+1 point to 100 and 200 respectively,
   // and print *ptr_i and *ptr_l
   // again, make sure there are no warnings
 
   *ptr_i = 0x64;
   *(ptr_i + 1) = 0xc8;
-
   printf("*ptr_i = %x, *ptr_l = %lx\n", *ptr_i, *ptr_l);
-  // TODO: clear the lower 13 bits of ptr_v
+  
+  // clear the lower 13 bits of ptr_v
   printf("BEFORE: ptr_v = %p\n", ptr_v);
   uintptr_t res = (uintptr_t) ptr_v;
   res &= ~((1UL << 13)-1);
