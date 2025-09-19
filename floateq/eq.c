c4cca8439536fc2b8dac6e4dd372e1e4fd196b91
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-09-19 14:26:39 -0400

    updated example

--- floateq/eq.c
+++ floateq/eq.c
@@ -2,14 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
+int floatEq(float a, float b){
+  return (fabs(a-b) <= 1.0e-7);
+}
+
 int main(){
+  union {
+    float fl;
+    unsigned char bytes[4];
+  } unVal;
+  
   float a = 0.1;
   float b = 0.8;
   float c = 0.9;
-  if (a + b == c)
-      printf("%f + %f == %f\n",a,b,a+b);
+  // print the concrete bytes of a + b
+  unVal.fl = a + b;
+  printf("Bytes of a + b = 0x");
+  for (int i=3; i >=0; i--){
+    printf("%x", unVal.bytes[i]);
+  }
+  putchar('\n');
+  // print the concrete bytes of c
+  unVal.fl = c;
+  printf("Bytes of c = 0x");
+  for (int i=3; i>=0; i--){
+    printf("%x", unVal.bytes[i]);
+  }
+  putchar('\n');
+  // check if a + b = c
+  if (floatEq(a+b, c))
+    printf("%f + %f == %f\n",a,b,c);
   else
-    printf("%f + %f != %f????\n",a,b,a+b);
+    printf("%f != %f????\n",a+b,c);
 
   return 0;
 }
