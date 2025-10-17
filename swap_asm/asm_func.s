cbab8b887defb284729d8cee56415aaf94bc57a6
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-10-17 17:35:35 -0400

    updated example

--- swap_asm/asm_func.s
+++ swap_asm/asm_func.s
@@ -1,5 +1,12 @@
 	.global swap_asm
-
+	.global set_asm
 swap_asm:
+	mov (%rdi), %edx
+	mov (%rsi), %ecx
+	mov %edx, (%rsi)
+	mov %ecx, (%rdi)
+	ret
 
+set_asm:
+	movl $2, (%rdi)
 	ret
--- swap_asm/swap.c
+++ swap_asm/swap.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 void swap_asm(int *a, int *b);
+void set_asm(int *a); // sets the value of a variable to 2
 
 int main(){
   int a = 100;
   int b = 200;
-
+  int c = 0;
   printf("a = %d, b = %d\n",a,b);
   swap_asm(&a,&b);
   printf("a = %d, b = %d\n",a,b);
+  set_asm(&c);
+  printf("c = %d\n", c);
 
   return 0;
 }
