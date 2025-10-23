a443074fcad84f43a7ffc82c6887b6754116b49a
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2025-10-23 12:04:47 -0400

    updated

--- add_asm/arith.c
+++ add_asm/arith.c
@@ -3,11 +3,13 @@
 
 int add_asm(int a, int b); // return sum of arguments a and b
 int inc_asm(int a); // return a + 1
-
+int pow_asm(int a, int b); // return a ** b
+  
 int main(int argc, char *argv[]){
   int a = atoi(argv[1]);
   int b = atoi(argv[2]);
   int res = add_asm(a,b);
   printf("sum of %d + %d = %d\n", a,b,res);
   printf("sum of (%d + %d) + 1 = %d\n", a,b,inc_asm(res));
+  printf("power %d ** %d = %d\n", a,b,pow_asm(a,b));
 }
--- add_asm/asm_func.s
+++ add_asm/asm_func.s
@@ -1,6 +1,7 @@
 .global add_asm
 .global inc_asm
-
+.global pow_asm
+	
 add_asm:
 	mov %edi,%eax
 	add %esi,%eax
@@ -9,4 +10,16 @@ add_asm:
 inc_asm:
 	mov %edi,%eax
 	add $1,%eax
+	ret
+
+pow_asm:
+	mov $1, %eax
+	mov $0, %edx
+	jmp .l1
+.l2:
+	imul %edi,%eax
+	add $1,%edx
+.l1:
+	cmp %edx,%esi
+	jg .l2
 	ret
